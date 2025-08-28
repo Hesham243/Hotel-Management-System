e398b773ed592f1dc5e0303b90b116c563219076
k7dbh <khaled.bujamal@gmail.com>
2025-08-28 15:44:56 +0300

    edit model to models

--- HMS_app/models.py
+++ HMS_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Hotel(model.Model):
+class Hotel(models.Model):
     name = models.CharField(max_length=100)
     rating = models.IntegerField()
     timezone = models.CharField(max_length=50)

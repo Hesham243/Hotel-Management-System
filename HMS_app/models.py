08d1480ba22df5fc4ea6dd290fb9c5951b14a503
HaiderMarhoon <haidooer2003@gmail.com>
2025-08-28 15:04:17 +0300

    make room modle

--- HMS_app/models.py
+++ HMS_app/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#======================= haider
+class Room(models.Model):
+    hotel_id = models.ForeignKey(Hotel, on_delete=CASCADE, related_name="rooms")
+    number = models.IntegerField(max_length=1200)
+    type = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    status = models.CharField(max_length=20, choices=[("available","Available")("booked","Booked")])
+    max_occupancy = models.IntegerField()
+    images = models.CharField()
+    
+    def __str__(self):
+        return self.name
+    

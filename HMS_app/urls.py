c8931cd5883cd2887bdba8211fca21073f3f31ee
Hesham243 <heshamahmed243@gmail.com>
2025-08-30 18:30:32 +0300

    create booking views and urls + templates in main

--- HMS_app/urls.py
+++ HMS_app/urls.py
@@ -3,5 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about, name="About")
+    path('about/', views.about, name="About"),
+
+    path('bookings/', views.booking_index, name='booking-index'),
+    path('bookings/<int:booking_id>/', views.booking_detail, name='booking-detail'),
+    path('bookings/create/', views.BookingCreateView.as_view(), name='booking-create'),
+    path('bookings/<int:pk>/edit/', views.BookingUpdateView.as_view(), name='booking-update'),
+    path('bookings/<int:pk>/delete/', views.BookingDeleteView.as_view(), name='booking-delete'),
 ]
+

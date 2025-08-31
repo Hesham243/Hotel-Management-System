c0c1fd884aae34c77d4fee6096698abffe9bba98
HaiderMarhoon <haidooer2003@gmail.com>
2025-08-31 09:32:16 +0300

    room tamp

--- HMS_app/urls.py
+++ HMS_app/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about, name="About")
+    path('about/', views.about, name="About"),
+    path('rooms/', views.rooms, name="Rooms"),
+    path("rooms/<int:room_id>", views.room_detail, name="room-detail")
 ]
--- HMS_app/views.py
+++ HMS_app/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Hotel,Room,Booking
 
 def home(request):
     return HttpResponse('<h1>Hello customer</h1>')
 
 def about(request):
     return render(request, 'about.html')
+
+def rooms(request):
+    rooms = Room.objects.all()
+    return render(request, 'room/index.html', {"rooms": rooms})
+
+def room_detail(request, room_id):
+    room = Room.objects.get(id=room_id)
+    return render (request, 'room/detail.html', {'room':room})
+    

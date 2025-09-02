e7c20870b12661c4461559046c00985b4ccb1099
HaiderMarhoon <haidooer2003@gmail.com>
2025-09-02 14:19:58 +0300

    Update settings.py

--- hotelcollecter/settings.py
+++ hotelcollecter/settings.py
@@ -79,8 +79,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'hotelcollector',
-        'USER': 'Hesham',
-        'PASSWORD': '123'
     }
 }
 
